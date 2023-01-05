95a73e9e86c222fd7d865c8145e3d1c1ac7bd92d
Velmurugan2195 <velroyalmech555@gmail.com>
2023-01-05 14:27:24 +0530

    gydg

---
+++ src/org/vel/Company.java
@@ -0,0 +1,5 @@
+package org.vel;
+
+public class Company {
+
+}
---
+++ src/org/vel/Hello.java
@@ -0,0 +1,20 @@
+package org.vel;
+
+public class Hello {
+	
+	private void tamil() {
+		System.out.println("tamil");
+
+	}
+	
+	private void english() {
+		System.out.println("english");
+
+	}
+	private void Hindi() {
+		System.out.println("hindi");
+
+	}
+	
+
+}
